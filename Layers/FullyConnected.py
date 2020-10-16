e3296dd7ba401e9e2b72160fb178f7d9bdf0ed5f
bigfishtwo <72951903+bigfishtwo@users.noreply.github.com>
2020-10-16 02:25:09 +0200

    Update FullyConnected.py

--- Layers/FullyConnected.py
+++ Layers/FullyConnected.py
@@ -8,15 +8,12 @@ class FullyConnected:
         self.input_tensor = None
         self.output_tensor = None
         self.weights = np.random.rand(self.input_size+1, self.output_size) - 0.5
-        # self.bias = np.random.rand(1,output_size) - 0.5#input_size plus one for the bias
         self.error = []
-        self.optimizer = None
 
     def forward(self, input_tensor):
         # returns the input tensor for the next layer
         # extend input matrix with bias
         self.input_tensor = np.concatenate((input_tensor, np.ones([input_tensor.shape[0], 1])), axis=1)
-        # self.input_tensor = input_tensor
         input_tensor = np.dot(self.input_tensor, self.weights)  #+ self.bias
         return input_tensor
 
@@ -25,11 +22,7 @@ class FullyConnected:
         self.error = error_tensor
         error_tensor = np.dot(self.error,self.weights.T)
         gradient_w = self.get_gradient_weights()
-        if self.optimizer:
-            self.weights = self.optimizer.calculate_update(self.weights,gradient_w)
-        else:
-            self.weights -= self.delta * gradient_w
-        # self.bias -= self.delta * self.error
+        self.weights -= self.delta * gradient_w
         error_tensor = np.delete(error_tensor,-1,1)
         return error_tensor
 
@@ -37,14 +30,3 @@ class FullyConnected:
         # returns the gradient with respect to the weights, after they have been calculated in the backward-pass.
         gradient_w = np.dot(self.input_tensor.T, self.error)
         return gradient_w
-
-
-    def initialize(self,weights_initializer,bias_initializer):
-        weight = self.weights[0:-1,:]
-        bias = self.weights[-1,:].reshape(1,self.weights.shape[1])
-        weight = weights_initializer.initialize(weight.shape,weight.ndim,weight.ndim)
-        bias = bias_initializer.initialize(bias.shape,bias.ndim,bias.ndim)
-        self.weights = np.concatenate((weight,bias),axis=0)
-
-    def set_optimizer(self,optimizer):
-        self.optimizer = optimizer
